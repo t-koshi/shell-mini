f65a14cbc06e89cc46ded435185429d852095f57
t-koshi <greapflutea2@yahoo.co.jp>
2020-02-16 01:28:34 +0900

    :+1: input line

---
+++ workspace/main.c
@@ -0,0 +1,125 @@
+/***************************************************************************//**
+  @file         main.c
+  @author       Stephen Brennan
+  @date         Thursday,  8 January 2015
+  @brief        LSH (Libstephen SHell)
+*******************************************************************************/
+
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/*
+  Function Declarations for builtin shell commands:
+ */
+int lsh_cd(char **args);
+int lsh_help(char **args);
+int lsh_exit(char **args);
+
+/*
+  List of builtin commands, followed by their corresponding functions.
+ */
+char *builtin_str[] = {
+};
+
+int (*builtin_func[]) (char **) = {
+};
+
+int lsh_num_builtins() {
+  return sizeof(builtin_str) / sizeof(char *);
+}
+
+/*
+  Builtin function implementations.
+*/
+
+/**
+   @brief Bultin command: change directory.
+   @param args List of args.  args[0] is "cd".  args[1] is the directory.
+   @return Always returns 1, to continue executing.
+ */
+int lsh_cd(char **args)
+{
+  return 1;
+}
+
+/**
+   @brief Builtin command: print help.
+   @param args List of args.  Not examined.
+   @return Always returns 1, to continue executing.
+ */
+int lsh_help(char **args)
+{
+  return 1;
+}
+
+/**
+   @brief Builtin command: exit.
+   @param args List of args.  Not examined.
+   @return Always returns 0, to terminate execution.
+ */
+int lsh_exit(char **args)
+{
+  return 0;
+}
+
+/**
+  @brief Launch a program and wait for it to terminate.
+  @param args Null terminated list of arguments (including program).
+  @return Always returns 1, to continue execution.
+ */
+int lsh_launch(char **args)
+{
+  return 1;
+}
+
+/**
+   @brief Execute shell built-in or launch program.
+   @param args Null terminated list of arguments.
+   @return 1 if the shell should continue running, 0 if it should terminate
+ */
+int lsh_execute(char **args)
+{
+  return 1;
+}
+
+#define LSH_RL_BUFSIZE 1024
+/**
+   @brief Read a line of input from stdin.
+   @return The line from stdin.
+ */
+char *sh_read_line(void)
+{
+  char *line = NULL;
+  size_t len = 0;
+  getline(&line, &len, stdin);
+  return line;
+}
+
+#define LSH_TOK_BUFSIZE 64
+#define LSH_TOK_DELIM " \t\r\n\a"
+/**
+   @brief Split a line into tokens (very naively).
+   @param line The line.
+   @return Null-terminated array of tokens.
+ */
+char **lsh_split_line(char *line)
+{
+}
+
+/**
+   @brief Loop getting input and executing it.
+ */
+void sh_loop(void)
+{
+  printf("%s", sh_read_line());
+}
+
+int main(void)
+{
+  sh_loop();
+  return EXIT_SUCCESS;
+}
